e295be90ee319588cb6140ae1281a8c8911c47b3
Christopher Cliff <chris@percolate.com>
2015-11-11 10:16:19 -0600

    Fix build fixture cleanup.

--- test/lib.spec.js
+++ test/lib.spec.js
@@ -1,13 +1,20 @@
+var fs = require('fs-extra')
 var lib = require('../')
 var path = require('path')
+
+var DESTINATION = path.resolve(__dirname, './fixtures/basic/build/')
 
 describe('lib', function () {
 
     this.timeout(20e3)
 
+    afterEach(function () {
+        fs.removeSync(DESTINATION)
+    })
+
     it('should do a basic build', function (done) {
         var options = {
-            destination: path.resolve(__dirname, './fixtures/basic/build/'),
+            destination: DESTINATION,
             schema: path.resolve(__dirname, './fixtures/basic/src/flatmarket.json'),
             source: path.resolve(__dirname, './fixtures/basic/src/'),
         }
